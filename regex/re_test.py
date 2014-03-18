66e7d897314315d0deed4c61fbce00dc6e5a074f
Kangwon Lee PF A 402 Wk03 <kangwon.lee@kpu.ac.kr>
2014-03-18 16:13:23 +0900

    added re.findall()

--- regex/re_test.py
+++ regex/re_test.py
@@ -1,3 +1,4 @@
+# -*- coding: cp949 -*-
 import re
 # regular expression
 
@@ -6,5 +7,9 @@ txt = f.read()
 f.close()
 
 # done reading file
+
+# '@dev.naver.com/' 와 같은 문자열을 포함하고 앞뒤로 다른 글자가 있는 모든 행을
+# txt 에서 찾는
+found = re.findall(".+@dev.naver.com/.+", txt)
 
 
