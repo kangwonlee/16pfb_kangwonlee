953099486a5fee840b4387695518b8ddc44c3c21
Kangwon Lee PF A 402 Wk03 <kangwon.lee@kpu.ac.kr>
2014-03-18 16:51:19 +0900

    pattern 개정으로 project id 만 추출

--- regex/re_test.py
+++ regex/re_test.py
@@ -10,7 +10,7 @@ f.close()
 
 # '@dev.naver.com/' 와 같은 문자열을 포함하고 앞뒤로 다른 글자가 있는 모든 행을
 # txt 에서 찾는
-found = re.findall(".+@dev.naver.com/.+", txt)
+found = re.findall(".+@dev.naver.com/git/(.+).git", txt)
 
 for item in found:
     print item
