96d228974259d912355c949e47526c284106357e
Kangwon Lee PF A 402 Wk03 <kangwon.lee@kpu.ac.kr>
2014-03-18 16:46:47 +0900

    연습 5 내용 추가

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -4,6 +4,25 @@
 
 # 각행 주석을 달아 보시오
 
+my_name = 'Kangwon Lee' # 각자 자신의 이름
+my_age = 25 # a lie
+my_height_cm = 180 # also a lie in centimeters
+my_weight_kg = 70 # 변수명에는 단위 명기 바람
+my_eyes = "Brown" # 동양인은 대부분 갈색
+my_teeth = "White"
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+# He -> She if appropriate
+print "He's %d centimeters tall." % my_height_cm
+print "He's %d kilograms heavy." % my_weight_kg
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % ( my_eyes, my_teeth )
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height_cm, my_weight_kg, my_age + my_height_cm + my_weight_kg)
 
 # 여기 까지 주석을 다 단 후 diff, commit
 
