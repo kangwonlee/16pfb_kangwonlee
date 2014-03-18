ea074ad4288bdc940f555b7aceda1f0269dca749
Kangwon Lee PF A 402 Wk03 <kangwon.lee@kpu.ac.kr>
2014-03-18 14:54:31 +0900

    study drill 1번에 따라 각 행 주석 추가

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -4,26 +4,39 @@
 # 참고문헌: http://learnpythonthehardway.org/book/ex3.html
 
 # 각행 주석을 달아 보시오
+
+# 이제부터 닭의 수를 셀 것임
 print "I will now count my chickens:"
 
+# 암탉의 수
 print "Hens", 25 + 30 / 6
+# 수탉의 수
 print "Roosters", 100 - 25 * 3 % 4
 
+# 이제부터는 달걀의 수를 셀 것임
 print "Now I will count the eggs:"
-
+# 달걀의 수 계산
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# 여기서부터는 비교 연산 예
 print "Is it true that 3 + 2 < 5 - 7?"
-
+# 아래 수식은 왼쪽의 결과값이 오른쪽 보다 작다면 참
 print 3 + 2 < 5 - 7
 
+# 위 식의 왼쪽 결과 표시
 print "What is 3 + 2?", 3 + 2
+# 위 식의 오른쪽 결과 표시
 print "What is 5 - 7?", 5 - 7
 
+# 비교 연산 결과 확인
 print "Oh, that's why it's False."
 
+# 비교 연산 예 추가
+# 왼쪽이 오른쪽 보다 크면 참
 print "Is it greater?", 5 > -2
+# 왼쪽이 오른쪽 이상이면 참
 print "Is it greater or equal?", 5 >= -2
+# 왼쪽이 오른쪽 이하면 참
 print "Is it less or equal?", 5 <= -2
 # 여기 까지 주석을 다 단 후 diff, commit
 
