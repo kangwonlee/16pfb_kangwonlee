2f0691fa9373baa6276e7a1e3514ea9a0442a4bb
Kangwon Lee PF A 402 Wk03 <kangwon.lee@kpu.ac.kr>
2014-03-18 14:48:52 +0900

    added "coding: cp949"; added comments on section, name, student id, reference, and study drills

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -1,3 +1,9 @@
+# -*- coding: cp949 -*-
+# 위 행의 역할은 이 파일이 한글 코드를 포함하고 있음을 표시하는 것임
+# [분반] [이름] [학번]
+# 참고문헌: http://learnpythonthehardway.org/book/ex3.html
+
+# 각행 주석을 달아 보시오
 print "I will now count my chickens:"
 
 print "Hens", 25 + 30 / 6
@@ -19,3 +25,6 @@ print "Oh, that's why it's False."
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
+# 여기 까지 주석을 다 단 후 diff, commit
+
+# 그 밖에 다른 study drills 항목들도 시도해 보면서 필요시 diff, commit
