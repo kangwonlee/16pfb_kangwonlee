580fb44babf183e19289ea16f6e373aebb8e14e5
Kangwon Lee PF A 402 Wk03 <kangwon.lee@kpu.ac.kr>
2014-03-18 15:58:37 +0900

    ex04/ex04.py 시작 참고문헌 : http://learnpythonthehardway.org/book/ex4.html

---
+++ ex04/ex04.py
@@ -0,0 +1,10 @@
+# -*- coding: cp949 -*-
+# [분반] [이름] [학번]
+# 참고문헌: http://learnpythonthehardway.org/book/ex4.html
+
+# 각행 주석을 달아 보시오
+
+
+# 여기 까지 주석을 다 단 후 diff, commit
+
+# 그 밖에 다른 study drills 항목들도 시도해 보면서 필요시 diff, commit
