895fde4fc07a5948546924fae24760eff3a8e864
Kangwon Lee <kangwon_fwd@naver.com>
2014-03-20 12:16:45 +0900

    project id 중복 조사

--- regex/re_test.py
+++ regex/re_test.py
@@ -1,16 +1,14 @@
 # -*- coding: cp949 -*-
 import re
-# regular expression
 
-f = open("140318pfa.txt", 'r')
+f = open("repo_list.txt", 'r')
 txt = f.read()
 f.close()
 
-# done reading file
-
-# '@dev.naver.com/' 와 같은 문자열을 포함하고 앞뒤로 다른 글자가 있는 모든 행을
-# txt 에서 찾는
-found = re.findall(".+@dev.naver.com/git/(.+).git", txt)
-
-for item in found:
-    print item
+# regular expression 을 이용하여 관심 문자열을 찾음
+found = re.findall("https://.+[/,](.+).git", txt)
+print "len(found) =", len(found)
+while found:
+    item = found.pop()
+    print item, 
+    print "\t[ duplicate =", item in found, ']'
