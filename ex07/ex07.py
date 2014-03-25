f44be2b97edbb19320ce0c47bd36bdbba99eb75a
Kangwon Lee PFA SY402 Wk04 <kangwon_fwd@naver.com>
2014-03-25 15:22:17 +0900

    입력 후 각자 실습 사항

--- ex07/ex07.py
+++ ex07/ex07.py
@@ -24,3 +24,9 @@ print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
 
 # 여기까지 입력 후 add, commit
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 열린게시판 / 오류노트 에 각자 오류노트 작성
