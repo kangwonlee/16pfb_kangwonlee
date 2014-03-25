ac3a421f39a7b5c45ea34f2815349161bee11003
Kangwon Lee PFA SY402 Wk04 <kangwon_fwd@naver.com>
2014-03-25 14:49:34 +0900

    urlretrieve() test

--- urllib/urllib_test.py
+++ urllib/urllib_test.py
@@ -5,3 +5,6 @@ url = "http://learnpythonthehardway.org/book/ex6.html"
 # hyper text transfer protocol
 
 urllib.urlretrieve(url, "myfile.html")
+
+url2 = "http://imgnews.naver.net/image/001/2014/03/24/GYH2014032400100004400_P2_59_20140324165907.jpg"
+urllib.urlretrieve(url2, "myfile2.jpg")
