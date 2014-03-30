0708272573598a007efc6c45df62f33418060f7b
Kang Won Lee <kangwon_fwd@naver.com>
2014-03-30 19:06:03 +0900

    added handle_path() : callback for os.path.walk()

--- regex/progress.py
+++ regex/progress.py
@@ -4,15 +4,37 @@ import os
 import pprint
 import re_test as ret
 
+def handle_path(arg, dirpath, namelist):
+    if ".git" not in dirpath:
+        print dirpath
+
 if "__main__" == __name__:
     # read file
     found = ret.get_proj_id_list(ret.txt_fname)
     
-    proj_id = found[4]
+    # sample git log example
+    '''git log --format="%h, %ai" [path]/[file]'''
     
+    '''
+    project id loop : for all project id's
+        go in
+        get list
+        if doesn't start with '.'
+            if file, get log
+                # commits
+                # begin ~ end
+                # average commit interval
+    '''
+    
+    proj_id = found[4]
+
+    # change directory to project repository    
     original_path = ret.cd_proj_repo(proj_id)
     
-    print os.listdir(os.curdir)
-    
+    # initialize student directory
+    student_dict = {}
+
+    os.path.walk(os.curdir, handle_path, student_dict)
+
     os.chdir(original_path)
     
