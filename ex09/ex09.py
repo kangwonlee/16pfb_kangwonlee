0506ad1458293d4332d4b65b7cb9908372e40b4f
Kangwon Lee PFA SY402 Wk05 <kangwon.lee@kpu.ac.kr>
2014-04-01 14:57:02 +0900

    entered ex09.py; ref: http://learnpythonthehardway.org/book/ex9.html

--- ex09/ex09.py
+++ ex09/ex09.py
@@ -2,6 +2,20 @@
 # [분반] [학번] [이름]
 # ref : http://learnpythonthehardway.org/book/ex9.html
 
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print '''
+There's something going on here.
+With the tree double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+'''
  
 # 여기까지 입력 후 add, commit
 
