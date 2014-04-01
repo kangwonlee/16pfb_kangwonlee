9409678bc211ad42bcdc0158f47cdba62b006c07
Kangwon Lee PFA SY402 Wk05 <kangwon.lee@kpu.ac.kr>
2014-04-01 15:58:30 +0900

    entered ex11 : http://learnpythonthehardway.org/book/ex11.html

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -2,6 +2,15 @@
 # [분반] [학번] [이름]
 # ref : http://learnpythonthehardway.org/book/ex11.html
 
+print "How young are you?(in year)"
+age_yr = raw_input()
+print "How tall are you?(in cm)",
+height_cm = raw_input()
+print "How much do you weigh?(in kg)",
+weight_kg = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age_yr, height_cm, weight_kg)
  
 # 여기까지 입력 후 add, commit
 
