70e312a2cad6c8be30e11bd0eb05176ce4e6e711
Kangwon Lee PFA SY402 Wk05 <kangwon.lee@kpu.ac.kr>
2014-04-01 15:02:33 +0900

    연습 10 입력 참고문헌 : http://learnpythonthehardway.org/book/ex10.html

--- ex10/ex10.py
+++ ex10/ex10.py
@@ -2,6 +2,21 @@
 # [분반] [학번] [이름]
 # ref : http://learnpythonthehardway.org/book/ex10.html
 
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
  
 # 여기까지 입력 후 add, commit
 
