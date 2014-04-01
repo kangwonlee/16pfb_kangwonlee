b26379ae31113f4d7f8a31855f0049eee9d3c152
Kangwon Lee PFA SY402 Wk05 <kangwon.lee@kpu.ac.kr>
2014-04-01 14:53:56 +0900

    ex09/ex09.py 시작

---
+++ ex09/ex09.py
@@ -0,0 +1,12 @@
+# -*- coding: cp949 -*-
+# [분반] [학번] [이름]
+# ref : http://learnpythonthehardway.org/book/ex9.html
+
+ 
+# 여기까지 입력 후 add, commit
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
+
+# 열린게시판 / 오류노트 에 각자 오류노트 작성
