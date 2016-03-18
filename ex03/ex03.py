a1ab0b9c6e68bf3e3584ec3280eb960fe48d0622
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-03-18 14:55:36 +0900

    ex03 : added Boolean link, corrected typo

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -9,7 +9,7 @@ print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
-print 3 + 2 < 5 - 7
+print 3 + 2 < 5 - 7  # Bool type result https://en.wikipedia.org/wiki/Boolean_data_type
 
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
@@ -20,4 +20,4 @@ print "How about some more."
 
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?"< 5 <= -2
+print "Is it less or equal?", 5 <= -2
