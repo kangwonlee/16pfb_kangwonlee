53159009345180178086f9410a78870a32a20622
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-01 15:10:54 +0900

    Study Drills 05.02 단위 변환

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,13 +1,16 @@
 name = 'Kangwon Lee'
 age = 25  # a lie
-height_cm = 184
+height_cm = 184.5
+cm_to_inch = 1.0 / 2.54
+height_inch = height_cm * cm_to_inch
 weight_kg = 80
 eyes = 'Brown'
 teeth = 'White'
 hair = 'Black'
 
 print "Let's talk about %s." % name
-print "He's %d cm tall." % height_cm
+print "He's %g cm tall." % height_cm
+print "He's %g inches tall." % height_inch
 print "He's %d kg heavy." % weight_kg
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
