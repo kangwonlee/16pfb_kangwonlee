4acab7ea6423686602b4a5c865f7c501fcd14b54
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-01 15:12:15 +0900

    python 3 에서도 실행 되도록 print 문에 () 추가

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -8,14 +8,14 @@ eyes = 'Brown'
 teeth = 'White'
 hair = 'Black'
 
-print "Let's talk about %s." % name
-print "He's %g cm tall." % height_cm
-print "He's %g inches tall." % height_inch
-print "He's %d kg heavy." % weight_kg
-print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (eyes, hair)
-print "His teeth are usually %s depending on the coffee." % teeth
+print("Let's talk about %s." % name)
+print("He's %g cm tall." % height_cm)
+print("He's %g inches tall." % height_inch)
+print("He's %d kg heavy." % weight_kg)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." % (eyes, hair))
+print("His teeth are usually %s depending on the coffee." % teeth)
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (
-    age, height_cm, weight_kg, age + height_cm + weight_kg)
+print("If I add %d, %d, and %d I get %d." % (
+    age, height_cm, weight_kg, age + height_cm + weight_kg))
