40fb544c2e20b8339f073d8fc4b4013560dc60e6
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-05 19:02:44 +0900

    \t 앞에 한 글자 추가

--- ex10/ex10.py
+++ ex10/ex10.py
@@ -2,7 +2,9 @@
 # http://learnpythonthehardway.org/book/ex10.html
 # 한글주석
 tabby_cat = "\tI'm tabbed in. 한글출력"
+a_tabby_cat = "a\tI'm tabbed in. 한글출력"
 space_cat = "    I'm tabbed in. 한글출력"
+a_space_cat = "a    I'm tabbed in. 한글출력"
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
@@ -14,7 +16,9 @@ I'll do a list:
 """
 
 print(tabby_cat)
+print(a_tabby_cat)
 print(space_cat)
+print(a_space_cat)
 print(persian_cat)
 print(backslash_cat)
 print('Study Drills 10.05\n %r' % fat_cat)
