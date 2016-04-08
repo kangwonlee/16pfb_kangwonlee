cb841fc7205ab3a64418deec80ddee5d8ec447a4
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-04-08 15:52:13 +0900

    Study Drills 13.02

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -3,8 +3,10 @@
 from sys import argv
 
 print("argv = " + str(argv))
-script, first, second = argv
+script, first, second, third, fourth = argv
 
 print("The script is called: %s" % script)
 print("Your first variable is : %s" % first)
 print("Your second variable is : %s" % second)
+print("Your third variable is : %s" % third)
+print("Your fourth variable is : %s" % fourth)
