264b4011944ca314653134525e9d496f9e5aff66
Kangwon Lee <kangwon_fwd@naver.com>
2016-04-27 12:02:17 +0900

    ex01.py additional instructions

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+# 참고문헌: http://learnpythonthehardway.org/book/ex1.html
+
 print("Hello World")
 print("Hello Again")
 print("I like typing this.")
@@ -9,3 +12,9 @@ print('I "said" do not touch this.')
 print("Kangwon Lee")
 print("16 03 11 15:11")
 print("Se Dol Lee : AlphaGo = 0:2")
+
+# 여기까지 입력 후 add, commit
+
+# 각 행 주석 입력 후 commit
+
+# 각자 Study drills 시도 후 필요시 commit
