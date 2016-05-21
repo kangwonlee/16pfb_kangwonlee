221efbb51f977d4cd2e67f2a90672eb86e372ea6
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-21 10:11:45 +0900

    entered ex35 code for python2 python3

--- ex35/ex35.py
+++ ex35/ex35.py
@@ -2,9 +2,12 @@
 # http://learnpythonthehardway.org/book/ex35.html
 from sys import exit
 
+if "raw_input" not in dir(__builtins__):
+    raw_input = input
+
 
 def gold_room():
-    print "This room is full of gold.  How much do you take?"
+    print("This room is full of gold. How much do you take?")
 
     choice = raw_input("> ")
     if "0" in choice or "1" in choice:
@@ -13,39 +16,39 @@ def gold_room():
         dead("Man, learn to type a number.")
 
     if how_much < 50:
-        print "Nice, you're not greedy, you win!"
+        print("Nice, you're not greedy, you win!")
         exit(0)
     else:
         dead("You greedy bastard!")
 
 
 def bear_room():
-    print "There is a bear here."
-    print "The bear has a bunch of honey."
-    print "The fat bear is in front of another door."
-    print "How are you going to move the bear?"
+    print("There is a bear here.")
+    print("The bear has a bunch of honey.")
+    print("The fat bear is in front of another door.")
+    print("How are you going to move th bear?")
     bear_moved = False
 
     while True:
         choice = raw_input("> ")
 
-        if choice == "take honey":
+        if "take honey" == choice:
             dead("The bear looks at you then slaps your face off.")
-        elif choice == "taunt bear" and not bear_moved:
-            print "The bear has moved from the door. You can go through it now."
+        elif "taunt bear" == choice and not bear_moved:
+            print("The bear has moved from the door. You can go through it now.")
             bear_moved = True
-        elif choice == "taunt bear" and bear_moved:
+        elif "taunt bear" == choice and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
-        elif choice == "open door" and bear_moved:
+        elif "open door" and bear_moved:
             gold_room()
         else:
-            print "I got no idea what that means."
+            print("I got no idea what that means.")
 
 
 def cthulhu_room():
-    print "Here you see the great evil Cthulhu."
-    print "He, it, whatever stares at you and you go insane."
-    print "Do you flee for your life or eat your head?"
+    print("Here you see the great evil Cthulhu.")
+    print("He, it, whatever stares you and you go insane.")
+    print("Do you flee for your life or eat your head?")
 
     choice = raw_input("> ")
 
@@ -58,26 +61,27 @@ def cthulhu_room():
 
 
 def dead(why):
-    print why, "Good job!"
+    print("%s, Good job!" % why)
     exit(0)
 
 
 def start():
-    print "You are in a dark room."
-    print "There is a door to your right and left."
-    print "Which one do you take?"
+    print("You are in a dark room.")
+    print("There is a door to your right and left.")
+    print("Which one do you take?")
 
     choice = raw_input("> ")
 
-    if choice == "left":
+    if "left" == choice:
         bear_room()
-    elif choice == "right":
+    elif "right" == choice:
         cthulhu_room()
     else:
         dead("You stumble around the room until you starve.")
 
 
-start()
+if __name__ == '__main__':
+    start()
 
 # 여기까지 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
