c6edda50092359c0624042db71f292bcb026f5e9
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-27 14:42:46 +0900

    ex39 : states dictionary

--- ex39/ex39.py
+++ ex39/ex39.py
@@ -1,6 +1,15 @@
 # -*-coding:utf8
 # http://learnpythonthehardway.org/book/ex39.html
 
+# create a mapping of state to abbreviation
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Hawaii': 'HI',
+}
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit  # 오류노트 에 각자 오류노트 작성
