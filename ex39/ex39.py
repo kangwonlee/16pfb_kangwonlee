d83ae5718cbee4288d4056afbb5deeda8235f0ab
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-27 14:48:56 +0900

    ex39 : print out every state abbreviation

--- ex39/ex39.py
+++ ex39/ex39.py
@@ -31,6 +31,13 @@ print('-' * 10)
 print("Hawaii's abbreviation is: %s" % states['Hawaii'])
 print("Florida's abbreviation is: %s" % states['Florida'])
 
+# print every state abbreviation
+print('-' * 10)
+print("states = %s" % states)
+print("states.items() = %s" % str(states.items()))
+print('-' * 10)
+for state, abbrev in states.items():
+    print("%s is abbreviated %s" % (state, abbrev))
 
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
