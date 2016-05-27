2b3d9727f1e53c77acf555deb39468b2966a1d63
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-27 15:54:51 +0900

    ex39  ex39_test: # print every state abbreviation

--- ex39/ex39_test.py
+++ ex39/ex39_test.py
@@ -36,6 +36,9 @@ print('-' * 10)
 print('Hawaii has: %s' % hashmap.get(cities, hashmap.get(states, 'Hawaii')))
 print('Florida has: %s' % hashmap.get(cities, hashmap.get(states, 'Florida')))
 
+# print every state abbreviation
+print('-' * 10)
+hashmap.list(states)
 
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
