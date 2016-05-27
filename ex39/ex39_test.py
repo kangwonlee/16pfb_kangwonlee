bf88b0a11fb8a8dc7612c8072b60f6617a21075c
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-05-27 15:49:59 +0900

    ex39  ex39_test: # create a set of states and some cities in them

--- ex39/ex39_test.py
+++ ex39/ex39_test.py
@@ -11,6 +11,17 @@ hashmap.set(states, 'California', 'CA')
 hashmap.set(states, 'New York', 'NY')
 hashmap.set(states, 'Hawaii', 'HI')
 
+# create a basic set of states and some cities in them
+cities = hashmap.new()
+hashmap.set(cities, 'CA', 'Los Angeles')
+hashmap.set(cities, 'HI', 'Honolulu')
+hashmap.set(cities, 'FL', 'Orlando')
+
+# add some more cities
+hashmap.set(cities, 'NY', 'New York')
+hashmap.set(cities, 'OR', 'Portland')
+
+
 # 입력 후 add, commit  # 각 행 주석 입력 후 commit
 
 # 각자 Study drills 시도 후 필요시 commit
