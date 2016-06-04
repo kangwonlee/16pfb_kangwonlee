e89f9e07f4dc16124dc9fc2f9f60f6f89dde8b6a
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-04 12:32:27 +0900

    ex47 import nose, game.Room, added test_room()

--- ex47/ex47.py
+++ ex47/ex47.py
@@ -2,6 +2,18 @@
 # http://learnpythonthehardway.org/book/ex47.html
 # 각 행 주석 입력
 
+from nose.tools import *
+
+from game import Room
+
+
+def test_room():
+    gold = Room("GoldRoom",
+                """This room has gold in it you can grab. There's a
+door to the north.""")
+
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
 
 # 입력 후 add, commit
 
