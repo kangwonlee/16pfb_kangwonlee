ddecbbe2efa23d9afc964ff2bc0f58188ac91735
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-04 12:34:45 +0900

    ex47 added test_room_paths()

--- ex47/ex47.py
+++ ex47/ex47.py
@@ -15,6 +15,18 @@ door to the north.""")
     assert_equal(gold.name, "GoldRoom")
     assert_equal(gold.paths, {})
 
+
+def test_room_paths():
+    center = Room("Center", "Test room in the center.")
+    north = Room("North", "Test room in the north.")
+    south = Room("North", "Test room in the south.")
+
+    center.add_paths({'north': north, 'south': south})
+
+    assert_equal(center.go('north'), north)
+    assert_equal(center.go('south'), south)
+
+
 # 입력 후 add, commit
 
 # 각자 Study drills 시도 후 필요시 commit
