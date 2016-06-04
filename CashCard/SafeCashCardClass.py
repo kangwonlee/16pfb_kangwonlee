517c350da5d87c18fbd002b6f9a5fd7ca08b9cdd
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-04 12:03:10 +0900

    Safe Cash Card Class : override withdraw()

--- CashCard/SafeCashCardClass.py
+++ CashCard/SafeCashCardClass.py
@@ -19,3 +19,17 @@ class SafeCashCard(CashCard):
         :return:
         """
         print("SafeCashCard withdraw()")  # 함수 호출 표식
+        # 잔고가 충분하면
+        if self.check_balance() >= amount_won:
+            # 출금한다
+            # 상위 클래스의 withdraw 메소드 호출
+            # http://stackoverflow.com/questions/805066/
+            CashCard.withdraw(self, amount_won)
+        # 그렇지 않으면
+        else:
+            # 오류를 표시한다
+            print("** 오류 발생 **")
+            print("잔고가 부족합니다")
+            print("인출되지 않았습니다")
+
+# SafeCashCard 클래스 정의 끝
