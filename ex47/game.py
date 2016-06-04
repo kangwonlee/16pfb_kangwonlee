0eee319528179c04c23f2c59524cebe4c4b408f6
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-04 12:31:32 +0900

    ex47 entered game.py Room class

--- ex47/game.py
+++ ex47/game.py
@@ -2,6 +2,18 @@
 # http://learnpythonthehardway.org/book/ex47.html
 # 각 행 주석 입력
 
+class Room(object):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.paths = {}
+
+    def go(self, directions):
+        return self.paths.get(directions, None)
+
+    def add_paths(self, paths):
+        self.paths.update(paths)
+
 # 입력 후 add, commit
 
 # 각자 Study drills 시도 후 필요시 commit
