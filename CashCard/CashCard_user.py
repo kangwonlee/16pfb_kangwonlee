9ddc582fa57045d5d38713c8801b9861b0497e8a
Kangwon LEE <kangwon.lee@kpu.ac.kr>
2016-06-04 11:02:55 +0900

    Cash Card user :chk_bal(), deposit()

--- CashCard/CashCard_user.py
+++ CashCard/CashCard_user.py
@@ -12,3 +12,13 @@ def chk_bal(message, account):
     :return:
     """
     print("%s : %d" % (message, account.check_balance()))
+
+
+# 현금 카드 모듈의 잔액 확인
+chk_bal("CashCard_module 잔액확인".CashCard_module)
+# 현금 카드에 10000원 입금
+print("10000원 입금")
+
+# CashCard.py 모듈 안의 deposit() 함수를 호출
+# CashCard.py 모듈 안의 balance_won 값이 증가
+CashCard_module.deposit(10000)
